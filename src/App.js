6fb6b5f8a9a284bdd006b402b6adaca56e7463b3
Caleb Zearing <czearing@outlook.com>
2020-03-24 12:23:30 -0700

    Content update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
 
-let data = [1, 2, 3, 3, 67, 8, 9];
-let counter = 0;
+let data1 = [1, 2, 1, 3, 4, 2, 9];
+let data2 = [1, 2, 1, 3, 4, 2, 9];
+let data3 = [1, 2, 1, 3, 4, 2, 9];
 
-function bubbleSort(data) {
+function bubbleSort(data1) {
+  let data = data1;
   let length = data.length;
+  let counter = 0;
   for (i = 0; i < length; i++) {
     counter++;
     for (x = 0; x < length; x++) {
@@ -20,14 +23,16 @@ function bubbleSort(data) {
     <div className="wrapper">
       <p>Sorted Items</p>
       {data}
-      <p>Number of swaps</p>
+      <p>Number of Swaps</p>
       {counter}
     </div>
   );
 }
 
-function selectionSort(data) {
+function selectionSort(data2) {
+  let data = data2;
   let length = data.length;
+  let counter = 0;
   for (i = 0; i < length; i++) {
     // current index = min
     let min = i;
@@ -40,12 +45,21 @@ function selectionSort(data) {
     }
     data[i] = data[min];
     data[min] = temp;
+    counter++;
   }
-  return data;
-  console.log(data);
+  return (
+    <div className="wrapper">
+      <p>Sorted Items</p>
+      {data}
+      <p>Number of Swaps</p>
+      {counter}
+    </div>
+  );
 }
 
-function insertionSort(data) {
+function insertionSort(data3) {
+  let counter = 0;
+  let data = data3;
   // length var
   let length = data.length;
   //repeat for every item
@@ -53,6 +67,7 @@ function insertionSort(data) {
     //if an item is greater than first index
     if (data[i] < data[0]) {
       //move number to the first position
+      counter++;
       [data[i], data[0]] = [data[0], data[i]];
     } else {
       //find number location
@@ -60,25 +75,32 @@ function insertionSort(data) {
         for (j = 0; j < length; j++) {
           if (data[i] >= data[j - 1] && data[i] < data[j]) {
             //move number to correct spot
+            counter++;
             data.splice(j, 0, data.splice(i, 1)[0]);
           }
         }
       }
     }
   }
-  console.log(data);
-  return data;
+  return (
+    <div className="wrapper">
+      <p>Sorted Items</p>
+      {data}
+      <p>Number of Swaps</p>
+      {counter}
+    </div>
+  );
 }
 
 export default function App() {
   return (
     <div className="App">
       <h1>Bubble Sort</h1>
-      {bubbleSort(data)}
+      {bubbleSort(data1)}
       <h1>Selection Sort</h1>
-      {selectionSort(data)}
+      {selectionSort(data2)}
       <h1>Insertion Sort</h1>
-      {insertionSort(data)}
+      {insertionSort(data3)}
     </div>
   );
 }
